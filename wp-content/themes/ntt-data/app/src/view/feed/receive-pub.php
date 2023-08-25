1cad6c0e9fe4f0cd8374fd74b06eb147469af53e
RafaelWBP <rafael.reis@webfoco.com>
2023-08-25 15:33:08 -0300

    Teste

--- wp-content/themes/ntt-data/app/src/view/feed/receive-pub.php
+++ wp-content/themes/ntt-data/app/src/view/feed/receive-pub.php
@@ -7,12 +7,15 @@
         </div>
         <div class="content">
             <div class="message">
-                <span>lOrem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat congue.</span>
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat congue.</span>
                 <div class="seeMore">Ver mais.</div>
             </div>
             <div class="action">
                 <div class="like">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/app/public\assets\img\icons/feed/heart.svg" alt="Imagem">
+                    <!--img src="<?php echo get_stylesheet_directory_uri(); ?>/app/public\assets\img\icons/feed/heart.svg" alt="Imagem"-->
+                    <svg width="22" height="19" viewBox="0 0 15 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2.08079 7.45061L5.35365 10.8021C6.53072 12.0074 8.46928 12.0074 9.64635 10.8021L12.9192 7.45061C14.3603 5.97494 14.3603 3.58242 12.9192 2.10675C11.4782 0.631083 9.14176 0.631084 7.70071 2.10675V2.10675C7.59064 2.21946 7.40936 2.21946 7.29929 2.10675V2.10675C5.85824 0.631084 3.52184 0.631083 2.08079 2.10675C0.639737 3.58242 0.639738 5.97494 2.08079 7.45061Z" stroke="#999999"/>
+                    </svg>
                     <div class="desc">Curtir</div>
                 </div>
                 <div class="comment">
@@ -37,8 +40,11 @@
                 <div class="seeMore">Ver mais.</div>
             </div>
             <div class="action">
-                <div class="like">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/app/public\assets\img\icons/feed/heart-full.svg" alt="Imagem">
+                <div class="like liked">
+                    <!--img src="<?php echo get_stylesheet_directory_uri(); ?>/app/public\assets\img\icons/feed/heart-full.svg" alt="Imagem"-->
+                    <svg width="22" height="19" viewBox="0 0 15 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2.08079 7.45061L5.35365 10.8021C6.53072 12.0074 8.46928 12.0074 9.64635 10.8021L12.9192 7.45061C14.3603 5.97494 14.3603 3.58242 12.9192 2.10675C11.4782 0.631083 9.14176 0.631084 7.70071 2.10675V2.10675C7.59064 2.21946 7.40936 2.21946 7.29929 2.10675V2.10675C5.85824 0.631084 3.52184 0.631083 2.08079 2.10675C0.639737 3.58242 0.639738 5.97494 2.08079 7.45061Z" stroke="#999999"/>
+                    </svg>
                     <div class="desc">Curtir</div>
                 </div>
                 <div class="comment">
@@ -61,7 +67,11 @@
             </div>
             <div class="action">
                 <div class="like">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/app/public\assets\img\icons/feed/heart-full.svg" alt="Imagem">
+                    <!--img src="<?php echo get_stylesheet_directory_uri(); ?>/app/public\assets\img\icons/feed/heart-full.svg" alt="Imagem"-->
+                    <svg width="22" height="19" viewBox="0 0 13 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1.08079 6.45061L4.35365 9.8021C5.53072 11.0074 7.46928 11.0074 8.64635 9.80209L11.9192 6.45061C13.3603 4.97494 13.3603 2.58242 11.9192 1.10675C10.4782 -0.368917 8.14176 -0.368916 6.70071 1.10675C6.59064 1.21946 6.40936 1.21946 6.29929 1.10675C4.85824 -0.368916 2.52184 -0.368917 1.08079 1.10675C-0.360263 2.58242 -0.360262 4.97494 1.08079 6.45061Z" fill="#DB0000"/>
+                    </svg>
+
                     <div class="desc">Curtir</div>
                 </div>
                 <div class="comment">
