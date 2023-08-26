018ebf0d0d9b629cd93ea4e470e6cf6235a34b7e
RafaelWBP <rafael.reis@webfoco.com>
2023-08-26 01:48:27 -0300

    Estilização Páginas Registro e Login

--- wp-content/themes/ntt-data/app/src/view/login.php
+++ wp-content/themes/ntt-data/app/src/view/login.php
@@ -15,42 +15,55 @@
     */
 get_header('login');    
 ?>
-    <form class="form-login" action="#" method="post" autocomplete="off">
-        <div class="login-card">
-            <div class="card-header">
-            </div>
-            <div class="card-body">
-                <?php include(TEMPLATE_PATH . '/messages.php'); 
-                ?>
-                <div class="form-group">
-                    <label for="email">E-mail</label>
-                    <input autocomplete="none" type="text" id="email" name="email"
-                     class="form-control 
-                    <?= isset($errors['email']) ? 'is-invalid' : ''; ?>" 
-                    value="<?=  isset($email) ? $email : ''; ?>" 
-                    placeholder="Informe o e-mail" autofocus>
-                    <div class="invalid-feedback">
-                        <?=  isset($errors['email']) ? $errors['email'] : '';
-                        ?>
+<section class="conteudo">
+    <div class="conteudo-container">
+        <form class="form-login" action="#" method="post" autocomplete="off">
+            <div class="login-card">
+                <div class="card-header">
+                    <h1>Lorem Ipsom Dolor</h1>
+                </div>
+                <div class="card-body">
+                    <?php include(TEMPLATE_PATH . '/messages.php'); 
+                    ?>
+                    <div class="form-group">
+                        <label for="email">E-mail</label>
+                        <div><input autocomplete="none" type="text" id="email" name="email"
+                        class="form-control 
+                        <?= isset($errors['email']) ? 'is-invalid' : ''; ?>" 
+                        value="<?=  isset($email) ? $email : ''; ?>" 
+                        placeholder="" autofocus></div>
+                        <div class="invalid-feedback">
+                            <?=  isset($errors['email']) ? $errors['email'] : '';
+                            ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Senha</label>
+                        <div><input autocomplete="off" type="text" id="password" name="password" 
+                        class="form-control <?= isset($errors['password']) ? 'is-invalid' : ''; ?>" 
+                        placeholder=""></div>
+                        <div class="invalid-feedback">
+                            <?=  isset($errors['password']) ? $errors['password'] : '';
+                            ?>
+                        </div>
                     </div>
                 </div>
-                <div class="form-group">
-                    <label for="password">Senha</label>
-                    <input autocomplete="off" type="text" id="password" name="password" 
-                    class="form-control <?= isset($errors['password']) ? 'is-invalid' : ''; ?>" 
-                    placeholder="Informe a Senha">
-                    <div class="invalid-feedback">
-                        <?=  isset($errors['password']) ? $errors['password'] : '';
-                        ?>
+                <div class="card-footer">
+                    <div class="form-group">
+                        <label></label>
+                        <div class="linha-links-login">
+                            <button class="btn btn-login">Entrar</button>
+                            <p>
+                                <a href="#">Esqueceu Senha</a>
+                                <a href="#">Cadastre-Se</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="card-footer">
-                <button class="btn btn-login">Entrar</button>
-            </div>
-        </div>
-    </form>
-
+        </form>
+    </div>
+</section>
 <?php    
 get_footer('login');
 /*
