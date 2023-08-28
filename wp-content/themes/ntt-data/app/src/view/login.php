ba3e2716fb7f33f1fa72e68496456c89eca0cb19
tawan <tawan.rio@webfoco.com>
2023-08-28 18:34:40 -0300

    alteração 325

--- wp-content/themes/ntt-data/app/src/view/login.php
+++ wp-content/themes/ntt-data/app/src/view/login.php
@@ -41,7 +41,7 @@ get_header('login');
                         <label for="password"><?=_t['login_senha']?></label>
                         <div><input autocomplete="off" type="text" id="password" name="password" 
                         class="form-control <?= isset($errors['password']) ? 'is-invalid' : ''; ?>" 
-                        placeholder="password"></div>
+                        placeholder=""></div>
                         <div class="invalid-feedback">
                             <?=  isset($errors['password']) ? $errors['password'] : '';
                             ?>
