b05a5cd4947ef7d9d67f9ac97616c4debe5c1bac
tawanrio <tawan.rio@webfoco.com>
2023-08-31 19:35:00 -0300

    agr sim

--- wp-content/themes/ntt-data/app/src/config/session.php
+++ wp-content/themes/ntt-data/app/src/config/session.php
@@ -2,7 +2,9 @@
 
 function requireValidSession(){
     $user = ($_SESSION) ? $_SESSION['user'] : null;
-    if (!isset($user) && $_SESSION['session_id'] != session_id()) {
+    $session_id = ($_SESSION['session_id']) ? $_SESSION['session_id'] : null;
+    
+    if (!isset($user) && $session_id != session_id()) {
         header('Location: login');
         exit();
     }
