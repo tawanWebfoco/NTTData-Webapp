deb0695a1a7230b2123b2a7d192c319291f8d486
tawan <tawan.rio@webfoco.com>
2023-09-28 09:52:35 -0300

    password

--- wp-content/themes/ntt-data/app/src/config/session.php
+++ wp-content/themes/ntt-data/app/src/config/session.php
@@ -2,19 +2,19 @@
 
 function requireValidSession(){
     $user = ($_SESSION) ? unserialize($_SESSION['user']) : null;
-    $user->password = null;
     // $session_id = ($_SESSION) ? $_SESSION['session_id'] : null;
     
     if (!isset($user)) {
         getUserJs();
         $user = ($_SESSION) ? unserialize($_SESSION['user']) : null;
-        $user->password = null;
+        
         if (!isset($user)) {
             // usleep(1000000); // 500000 microssegundos = 500 milissegundos
         // header('Location: login');
         exit();
         }
     }
+    $user->password = null;
 }
 
 
